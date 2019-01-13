0001554c15d679078b68ce7a7f4cae216bc4f9f5
Fioringo <shtuk.andrey@gmail.com>
2019-01-13 02:34:08 -0500

    minor modifications, still doesn't work as intended

--- Assignment 1/count-down.js
+++ Assignment 1/count-down.js
@@ -1,26 +1,44 @@
-
+let totalSeconds;
+let interval;
+let renderTime;
 
 function startTimer(){
+	clearInterval(interval);
+	clearInterval(renderTime)
 	let hours = parseInt(document.getElementById('hoursIn').value);
 	let minutes = parseInt(document.getElementById('minutesIn').value);
-	let seconds = parseInt(document.getElementById('secondsIn').value);
-	// let hoursOut = document.getElementById('hours');
-	// let minutesOut = document.getElementById('minutes');
-	// let secondsOut = document.getElementById('seconds').value;
+	let seconds = parseFloat(document.getElementById('secondsIn').value);
 
-	let totalSeconds = hours * 360 + minutes * 60 + seconds;
-
-	totalReturn = countDown(totalSeconds);
+	totalSeconds = hours * 3600 + minutes * 60 + seconds;
+	
+	renderTime = totalSeconds;
+	// console.log(renderTime%360);
+	// document.getElementById('hours').innerHTML=(parseInt(renderTime/360)+":")
+	// renderTime = renderTime%360;
+	// document.getElementById('minutes').innerHTML=(parseInt(renderTime/60)+":");
+	// renderTime = renderTime%60;
+	// document.getElementById('seconds').innerHTML=(parseInt(renderTime));
+	interval = setInterval(countDown, 1000);
+	// document.getElementById('seconds').innerHTML=(totalSeconds);
+	// console.log(totalSeconds.toString() + " " + totalSeconds.toString());
 
 	// document.getElementById('seconds') = totalSeconds;	
 
-	() => document.getElementById('secondsIn').value('10');
 	// totalSeconds.toString()
 
 }
 
-function countDown(s){
-	if(s <= 0)
-		return 0;
-	return s - 1;
+function countDown(){
+	if(totalSeconds <= 0)
+		totalSeconds = 0;
+	else
+		totalSeconds = totalSeconds - 1;
+	// renderTime = totalSeconds;
+	// document.getElementById('hours').innerHTML=(parseInt(renderTime/360)+":")
+	// renderTime = renderTime%360;
+	// document.getElementById('minutes').innerHTML=(parseInt(renderTime/60)+":");
+	// renderTime = renderTime%60;
+	// document.getElementById('seconds').innerHTML=(parseInt(renderTime / 60));
+	// interval = setInterval(countDown, 1000);
+	document.getElementById('seconds').innerHTML=(totalSeconds);
 }
